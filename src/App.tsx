12303255688aedd49a0db93fbd9e84a2ba992374
k-arikawa0312 <s1F102402710@iniad.org>
2024-09-26 20:40:10 +0900

    修正

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,12 @@ function App() {
   return (
     <div className="form-container">
       <form>
-        <label htmlFor="name">Name</label>
-        <input type="text" id="name" name="name" />
-        <label htmlFor="password">Password</label>
-        <input type="password" id="password" name="password" />
-        <label htmlFor="email">Email</label>
-        <input type="text" id="email" name="email" />
+        <label htmlFor="名前">名前</label>
+        <input type="text" id="name" name="名前" />
+        <label htmlFor="パスワード">パスワード</label>
+        <input type="password" id="password" name="パスワード" />
+        <label htmlFor="メールアドレス">メールアドレス</label>
+        <input type="email" id="email" name="メールアドレス" />
       </form>
     </div>
   );
