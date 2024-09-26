5d736390900a97d3119874a1037ae3fc1babfea3
k-arikawa0312 <s1F102402710@iniad.org>
2024-09-26 20:35:59 +0900

    front

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,11 @@ function App() {
     <div className="form-container">
       <form>
         <label htmlFor="name">Name</label>
+        <input type="text" id="name" name="name" />
+        <label htmlFor="password">Password</label>
+        <input type="password" id="password" name="password" />
+        <label htmlFor="email">Email</label>
+        <input type="text" id="email" name="email" />
       </form>
     </div>
   );
