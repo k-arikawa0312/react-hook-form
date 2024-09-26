5f525eba45e5bb46d5353a2eeea0250a44f5f144
k-arikawa0312 <s1F102402710@iniad.org>
2024-09-26 20:32:48 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function App() {
+  return (
+    <div className="form-container">
+      <form>
+        <label htmlFor="name">Name</label>
+      </form>
+    </div>
+  );
+}
+
+export default App;
